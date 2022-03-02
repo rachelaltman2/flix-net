53f3bafb3c5e52a264f1fbcac4bbcc3468a2ab17
Rachel Altman <62564584+rachelxaltman@users.noreply.github.com>
2022-03-02 00:45:14 -0500

    Fixed validator to add User Name

--- middleware/validator.js
+++ middleware/validator.js
@@ -17,6 +17,7 @@ exports.validateId = (req, res, next)=>{
 
 exports.validateSignUp = [body('firstName', 'First Name cannot be empty').notEmpty().trim().escape(),
 body('lastName', 'Last Name cannot be empty').notEmpty().trim().escape(),
+body('userName', 'User Name cannot be empty').notEmpty().trim().escape(),
 body('email', 'Email must be a valid email address').isEmail().trim().escape().normalizeEmail(),
 body('password', 'Password must be at least 8 characters and at most 64 characters').isLength({min: 8, max: 64})];
 
